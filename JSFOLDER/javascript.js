8eac87b13ff109b1b73636d64b4901f230781dbc
Kajal Rajak <rajakkajal219@gmail.com>
2025-12-11 16:27:47 +0530

    create header section

---
+++ JSFOLDER/javascript.js
@@ -0,0 +1,10 @@
+const search =document.querySelector('.search-icon');
+const searchForm =document.querySelector('.search-form');
+const searchInput = document.querySelector('.search-input');
+
+//i use this it will show
+search.addEventListener("click",()=>{
+searchForm.classList.toggle("active")
+})
+
+
