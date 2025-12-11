94dbfbaa2f70300e4cf1650321338202c85519d3
Kajal Rajak <rajakkajal219@gmail.com>
2025-12-11 22:00:51 +0530

    insert our cakes

--- JSFOLDER/javascript.js
+++ JSFOLDER/javascript.js
@@ -7,4 +7,11 @@ search.addEventListener("click",()=>{
 searchForm.classList.toggle("active")
 })
 
+const cartIcon =document.querySelector('.cart-icon');
+const cartItems =document.querySelector('.cart-items-conatiner');
 
+
+//i use this it will show cart item
+cartIcon.addEventListener("click",()=>{
+cartItems.classList.toggle("active")
+})
